3ab15372fd6d1c37bb4726890d4e7d2755f95537
Luccas Costa Specht <luccas.specht@cwi.com.br>
2022-01-29 22:24:21 -0300

    feat(scss): initial style list dragons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { LoggedInUserProvider } from '~/contexts';
 
-import '~/styles/globals.css';
+import '~/styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <LoggedInUserProvider>
+    <Head>
+      <meta charSet="utf-8" name="viewport" />
+    </Head>
     <Component {...pageProps} />
   </LoggedInUserProvider>
 );
